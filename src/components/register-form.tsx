04bdfe19966dd278eaadf5a6e476ba64c16fef9f
Rodrigo Pérez <rodrigop_23@outlook.es>
2024-10-30 18:27:42 -0500

    feat: show/hide password in inputs

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -24,20 +24,20 @@ export default function RegisterForm() {
           <div className="grid grid-cols-2 gap-4">
             <div className="grid gap-2">
               <Label htmlFor="first-name">Nombre</Label>
-              <Input id="first-name" required />
+              <Input id="first-name" name="first_name" required />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="last-name">Apellido</Label>
-              <Input id="last-name" required />
+              <Input id="last-name" name="last_name" required />
             </div>
           </div>
           <div className="grid gap-2">
             <Label htmlFor="email">Correo electrónico</Label>
-            <Input id="email" type="email" required />
+            <Input id="email" type="email" name="email" required />
           </div>
           <div className="grid gap-2">
             <Label htmlFor="password">Contraseña</Label>
-            <Input id="password" type="password" />
+            <Input id="password" name="password" type="password" />
           </div>
           <Button type="submit" className="w-full">
             Crear una cuenta
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
+import { Eye, EyeOff } from "lucide-react";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface InputProps
@@ -8,6 +11,36 @@ export interface InputProps
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
+    const [show, setShow] = React.useState(false);
+
+    if (type === "password") {
+      return (
+        <div className="relative w-full">
+          <input
+            autoComplete="off"
+            type={!show ? type : "text"}
+            className={cn(
+              "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+              className,
+            )}
+            ref={ref}
+            {...props}
+          />
+          <button
+            onClick={() => setShow((prev) => !prev)}
+            className="absolute right-3 top-1/2 -translate-y-1/2 transform"
+            type="button"
+          >
+            {show ? (
+              <Eye className="stroke-slate-700/70" size={18} />
+            ) : (
+              <EyeOff className="stroke-slate-700/70" size={18} />
+            )}
+          </button>
+        </div>
+      );
+    }
+
     return (
       <input
         type={type}
