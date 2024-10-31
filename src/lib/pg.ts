55906ec02d46d3b7d8e442f5cf3abcd3c6991b3a
Rodrigo Pérez <rodrigop_23@outlook.es>
2024-10-31 13:26:35 -0500

    feat: add pg client to try sql injection

---
+++ src/lib/pg.ts
@@ -0,0 +1,37 @@
+// lib/db.ts
+import { Pool, QueryResultRow } from "pg";
+
+let instance: Database | null = null;
+
+class Database {
+  private pool: Pool;
+
+  private constructor() {
+    this.pool = new Pool({
+      user: process.env.PG_USER,
+      host: process.env.PG_HOST,
+      database: process.env.PG_DATABASE,
+      password: process.env.PG_PASSWORD,
+      port: Number(process.env.PG_PORT),
+    });
+  }
+
+  public static getInstance(): Database {
+    if (!instance) {
+      instance = new Database();
+    }
+    return instance;
+  }
+
+  public async query<T extends QueryResultRow>(text: string): Promise<T[]> {
+    const client = await this.pool.connect();
+    try {
+      const result = await client.query<T>(text, []);
+      return result.rows;
+    } finally {
+      client.release();
+    }
+  }
+}
+
+export default Database.getInstance();
